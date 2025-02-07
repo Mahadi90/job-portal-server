e28c0160f97ee0676c083e05095d3fddfb275df5
Mahadi Hasan <mahadimhs787@gmail.com>
2025-02-07 11:48:47 +0600

    details api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -34,6 +34,13 @@ async function run() {
 
    app.get('/jobs', async(req, res) =>{
     const result = await jobsCollection.find().toArray();
+    res.json(result)
+   })
+
+   app.get('/jobs/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await jobsCollection.findOne(query)
     res.json(result)
    })
 
